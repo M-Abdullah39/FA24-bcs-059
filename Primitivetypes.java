4347b54770a57285d75626e6e03e79a8cbd23646
M-Abdullah39 <fa24-bcs-059@cuilahore.edu.pk>
2025-02-13 16:56:21 +0500

    Add files via upload

---
+++ Primitivetypes.java
@@ -0,0 +1,10 @@
+public class Primitivetypes{
+	 public static void main(String[] args){
+		 int i=5;
+		 short s=3;
+		 long l=4920;
+		 char ch='N';
+		 boolean bool=true;
+		 System.out.println("Integer: "+i);
+	 }
+}
